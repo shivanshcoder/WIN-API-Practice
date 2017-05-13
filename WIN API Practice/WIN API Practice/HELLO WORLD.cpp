78088e0bfee14641823505e1a076785690424674
Shivansh Mathur <mathurshiva90@outlook.com>
2017-05-14 00:40:52 +0530

    HELLO WORLD

---
+++ WIN API Practice/WIN API Practice/HELLO WORLD.cpp
@@ -0,0 +1,8 @@
+#include<windows.h>
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdshow)
+{
+	MessageBox(NULL, TEXT("Hello Wolrd"), TEXT("First WIN API"), 0);
+	return 0; 
+}
